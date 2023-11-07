7e9b895472228ac338128968be24d456ad2641dd
VyThao <Thaonguyenvy109@gmail.com>
2023-11-07 20:11:43 +0700

    'SignInSignUpMovePointer'

--- ChatApp/ClientWinform/Login.cs
+++ ChatApp/ClientWinform/Login.cs
@@ -12,11 +12,32 @@ namespace ClientWinform
 {
     public partial class loginForm : Form
     {
+        private bool mouseDown;
+        private Point lastLocation;
         public loginForm()
         {
             InitializeComponent();
         }
-         private void loginForm_Load(object sender, EventArgs e)
+        private void loginForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void loginForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void loginForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+        private void loginForm_Load(object sender, EventArgs e)
         {
             this.Size = new Size(780, 560);
 
--- ChatApp/ClientWinform/SignUpForm.cs
+++ ChatApp/ClientWinform/SignUpForm.cs
@@ -12,9 +12,30 @@ namespace ClientWinform
 {
     public partial class SignUpForm : Form
     {
+        private bool mouseDown;
+        private Point lastLocation;
         public SignUpForm()
         {
             InitializeComponent();
+        }
+        private void SignUpForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void SignUpForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void SignUpForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
         }
         private void SignUpForm_Load(object sender, EventArgs e)
         {
