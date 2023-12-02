f67754254b9c81cb1a5f6e2a2f56836b220dcb62
VyThao <Thaonguyenvy109@gmail.com>
2023-12-02 21:22:38 +0700

    NavigationModify

--- ChatApp/ClientWinform/View/User/NavigationForm.cs
+++ ChatApp/ClientWinform/View/User/NavigationForm.cs
@@ -25,6 +25,9 @@ namespace ClientWinform
 
         private User user = new User();
         byte[] images = null;
+
+        ChatListForm chatForm = null;
+        ProfileForm profileForm = null;
         public NavigationForm()
         {
             InitializeComponent();
@@ -34,10 +37,101 @@ namespace ClientWinform
             this.user = user;
             showDetail(user);
             ActiveButton((Guna2Button)chatBtn);
-            ChatListForm f = new ChatListForm(user.Id);
-            OpenStartForm(f);
+            chatForm = new ChatListForm(user.Id);
+            profileForm = new ProfileForm(user);
+            OpenStartForm(chatForm);
         }  
+        public void showDetail(User user)
+        {
+            lableUsername.Text = user.Username;
+            images = BLL.UserBLL.getAvaLinkById((Nullable<System.Int32>)user.IdAvatar);
+            if (images == null)
+            {
+                pictureAva.Image = Resources.defaultAvatar;
+            }
+            else
+            {
+                MemoryStream mstream = new MemoryStream(images);
+                pictureAva.Image = Image.FromStream(mstream);
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control ctrl in panelBtn.Controls)
+            {
+                if (ctrl is Guna2Button)
+                {
+                    Guna2Button btn = (Guna2Button)ctrl;
+                    btn.FillColor = Color.FromArgb(122, 151, 244);
+                }
+            }
+        }
+        private void ActiveButton(object btnSender)
+        {
+            DisableButton();
+            if (btnSender != null)
+            {
+                currentBut = (Guna2Button)btnSender;
+                currentBut.FillColor = Color.FromArgb(96, 127, 244);
 
+                
+                panelLeftMenu.Visible = true;
+                panelLeftMenu.Location = new Point(currentBut.Location.X, currentBut.Location.Y);
+                panelLeftMenu.Size = new Size(4, currentBut.Size.Height);
+            }
+        }
+        public void OpenStartForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelChild.Controls.Add(childForm);
+            this.panelChild.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            
+        }
+        public void OpenChilForm(Form childForm, object sender)
+        {
+            ActiveButton(sender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelChild.Controls.Add(childForm);
+            this.panelChild.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Activate();
+            childForm.Show();
+        }
+        private void chatBtn_Click(object sender, EventArgs e)
+        {
+            OpenChilForm(chatForm, sender);
+        }
+
+        private void exploreBtn_Click(object sender, EventArgs e)
+        {
+            ActiveButton(sender);
+            panelExplore.Size = new Size(300, panelChild.Height);
+            panelExplore.Location = new Point(0, 0);
+            panelExplore.Visible = true;
+            panelExplore.BringToFront();
+        }
+        private void exploreUserPanel_Click(object sender, EventArgs e)
+        {
+            ProfileExplorerForm f = new ProfileExplorerForm();
+            f.ShowDialog();
+        }
+        private void profileBtn_Click(object sender, EventArgs e)
+        {
+            OpenChilForm(profileForm, sender);
+            profileForm.del += new ProfileForm.MyDel(showDetail);
+        }
         private void loginForm_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
@@ -57,107 +151,13 @@ namespace ClientWinform
         {
             mouseDown = false;
         }
-        public void showDetail(User user)
-        {
-            lableUsername.Text = user.Username;
-            images = BLL.UserBLL.getAvaLinkById((Nullable<System.Int32>)user.IdAvatar);
-            if (images == null)
-            {
-                pictureAva.Image = Resources.defaultAvatar;
-            }
-            else
-            {
-                MemoryStream mstream = new MemoryStream(images);
-                pictureAva.Image = Image.FromStream(mstream);
-            }
-        }
-        private void profileBtn_Click(object sender, EventArgs e)
-        {
-            ProfileForm f = new ProfileForm(user);
-            OpenChilForm(f, sender);
-            f.del += new ProfileForm.MyDel(showDetail);
-        }
-        private void ActiveButton(object btnSender)
-        {
-            DisableButton();
-            if (btnSender != null)
-            {
-                currentBut = (Guna2Button)btnSender;
-                currentBut.FillColor = Color.FromArgb(96, 127, 244);
-
-                
-                panelLeftMenu.Visible = true;
-                panelLeftMenu.Location = new Point(currentBut.Location.X, currentBut.Location.Y);
-                panelLeftMenu.Size = new Size(4, currentBut.Size.Height);
-            }
-        }
-        private void DisableButton()
-        {
-            foreach (Control ctrl in panelBtn.Controls)
-            {
-                if (ctrl is Guna2Button)
-                {
-                    Guna2Button btn = (Guna2Button)ctrl;
-                    btn.FillColor = Color.FromArgb(122, 151, 244);
-                }
-            }
-        }
-
-        public void OpenStartForm(Form childForm)
-        {
-            if (activeForm != null)
-            {
-                activeForm.Close();
-            }
-            activeForm = childForm;
-            childForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-            this.panelChild.Controls.Add(childForm);
-            this.panelChild.Tag = childForm;
-            childForm.BringToFront();
-            //childForm.Activate();
-            childForm.Show();
-            
-        }
-        public void OpenChilForm(Form childForm, object sender)
-        {
-            //if (activeForm != null)
-            //{
-            //    activeForm.Close();
-            //}
-            ActiveButton(sender);
-            activeForm = childForm;
-            childForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-            this.panelChild.Controls.Add(childForm);
-            this.panelChild.Tag = childForm;
-            childForm.BringToFront();
-            childForm.Activate();
-            childForm.Show();
-        }
-
-        private void chatBtn_Click(object sender, EventArgs e)
-        {
-            ChatListForm f = new ChatListForm(user.Id);
-            OpenChilForm(f, sender);
-        }
 
 
-        private void exploreBtn_Click(object sender, EventArgs e)
-        {
-            ActiveButton(sender);
-            panelExplore.Size = new Size(300, panelChild.Height);
-            panelExplore.Location = new Point(0, 0);
-            panelExplore.Visible = true;
-            panelExplore.BringToFront();
-        }
-        private void exploreUserPanel_Click(object sender, EventArgs e)
-        {
-            ProfileExplorerForm f = new ProfileExplorerForm();
-            f.ShowDialog();
-        }
+
+
+
+
+
 
 
     }
