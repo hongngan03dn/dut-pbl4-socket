41c87b4d31f939dc5c4ca9b9456e028023b52995
VyThao <Thaonguyenvy109@gmail.com>
2023-12-04 15:41:24 +0700

    ShowOnlineListNewOld

--- ChatApp/ClientWinform/SocketHandles/MailClient.cs
+++ ChatApp/ClientWinform/SocketHandles/MailClient.cs
@@ -19,19 +19,19 @@ namespace ClientWinform.SocketHandles
 {
     public class MailClient
     {
-        delegate void setForm(string msg, Form chatListForm);
-        static String _ipServer = "192.168.1.21";
+        delegate void setForm(string[] msg, Form chatListForm);
+        static String _ipServer = "192.168.2.17";
         static int _port = 6767;
         static IPEndPoint _ipep;
         static Socket _client;
 
-        static User user;
+        static User userLoggined;
         static int idLoggined = 0;
         public static void connectServer(int myId, String username, Form activeForm)
         {
             _ipep = new IPEndPoint(IPAddress.Parse(_ipServer), _port);
             _client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            user = BLL.UserBLL.getUserByID(myId);
+            userLoggined = BLL.UserBLL.getUserByID(myId);
 
 
             try
@@ -70,89 +70,93 @@ namespace ClientWinform.SocketHandles
                 throw ex;
             }
         }
-        public static void UpdateListChat(string message, Form activeForm)
+        public static void UpdateListChat(string[] message, Form activeForm)
         {
-            bool isParsableToId = int.TryParse(message, out int myId);
+            string[] partsLogin = message[0].Split(' ');
+            bool isParsableToId = int.TryParse(partsLogin[0], out int myId);
             
             int[] idOnlines = null;
             if (isParsableToId)
             {
                 idLoggined = myId;
             }
+            string keyword = "Current onlines: "; //message = Current onlines: 2, 3, 4
+
+            string onlyIdNumbers = "";
+
+            string[] parts = message[1].Split(new[] {keyword}, StringSplitOptions.None);
+            if (parts.Length == 2)
+            {
+                if (parts[0] != "")
+                    idLoggined = int.Parse(parts[0].Trim());
+
+                onlyIdNumbers = parts[1];
+            }
+            string[] onlineStrings = onlyIdNumbers.Trim().Split(',');
+
+            idOnlines = new int[onlineStrings.Length];
+            for (int i = 0; i < onlineStrings.Length; i++)
+            {
+                if (onlineStrings[i].Trim() != "" && int.Parse(onlineStrings[i].Trim()) != idLoggined)
+                {
+                    idOnlines[i] = int.Parse(onlineStrings[i].Trim());
+                }
+            }
+            Array.Resize(ref idOnlines, idOnlines.Length - 1);
+
+            //delegate
+            if (activeForm.InvokeRequired)
+            {
+                setForm d = new setForm(UpdateListChat);
+                activeForm.Invoke(d, new object[] { message, activeForm });
+            }
             else
             {
-                string keyword = "Current onlines: ";
 
-                string onlyIdNumbers = "";
-
-                string[] parts = message.Split(new[] {keyword}, StringSplitOptions.None);
-                if (parts.Length == 2)
+                if (activeForm is NavigationForm navigationForm && idLoggined != 0)
                 {
-                    if (parts[0] != "")
-                        idLoggined = int.Parse(parts[0].Trim());
-
-                    onlyIdNumbers = parts[1];
-                }
-                string[] onlineStrings = onlyIdNumbers.Trim().Split(',');
-
-                idOnlines = new int[onlineStrings.Length];
-                for (int i = 0; i < onlineStrings.Length; i++)
-                {
-                    if (int.Parse(onlineStrings[i].Trim()) != idLoggined)
+                    List<DTO.User> users = BLL.UserBLL.getUserListChat(userLoggined.Id);
+                    foreach (DTO.User user in users)
                     {
-                        idOnlines[i] = int.Parse(onlineStrings[i].Trim());
-                    }
-                }
-                Array.Resize(ref idOnlines, idOnlines.Length - 1);
-
-                //delegate
-                if(activeForm.InvokeRequired)
-                {
-                    setForm d = new setForm(UpdateListChat);
-                    activeForm.Invoke(d, new object[] { message, activeForm });
-                }
-                else
-                {
-
-                    if (activeForm is NavigationForm navigationForm && idLoggined != 0)
-                    {
-                        List<DTO.User> users = BLL.UserBLL.getUserListChat(user.Id);
-                        byte[] images = null;
-                        foreach (DTO.User user in users)
+                        navigationForm.chatForm.Invoke((MethodInvoker)delegate
                         {
-                            ChatReviewForm chat = new ChatReviewForm();
-                            images = BLL.UserBLL.getAvaLinkById((Nullable<System.Int32>)user.IdAvatar);
-                            if (images == null)
-                            {
-                                chat.ava = Resources.defaultAvatar;
-                            }
-                            else
-                            {
-                                MemoryStream mstream = new MemoryStream(images);
-                                chat.ava = Image.FromStream(mstream);
-                            }
-                            if(user.Id == idLoggined)
-                            {
-                                chat.isPictureBoxOnlineVisible = true;
-                            }
-                            if (idOnlines != null)
-                            {
-                                for (int i = 0; i < idOnlines.Length; i++)
-                                {
-                                    if(user.Id == idOnlines[i])
-                                        chat.isPictureBoxOnlineVisible = true;
-                                }
-                            }
-                            chat.userName = user.Username;
-                            navigationForm.chatForm.Invoke((MethodInvoker)delegate {
-
-                                foreach (Control c in chat.Controls)
-                                {
-                                    c.Click += new EventHandler(navigationForm.chatForm.chatPanel_Click);
-                                }
-                                navigationForm.chatForm.flowLayoutPanelListChat.Controls.Add(chat);
-                            });
+                            ChatReviewForm chat = navigationForm.chatForm.flowLayoutPanelListChat.Controls.OfType<ChatReviewForm>()
+                                                                                                      .FirstOrDefault(c => c.userName == user.Username);
+                        if (chat == null)
+                        {
+                            chat = new ChatReviewForm();
+                            navigationForm.chatForm.flowLayoutPanelListChat.Controls.Add(chat);
                         }
+                        byte[] images = BLL.UserBLL.getAvaLinkById((Nullable<System.Int32>)user.IdAvatar);
+                        if (images == null)
+                        {
+                            chat.ava = Resources.defaultAvatar;
+                        }
+                        else
+                        {
+                            MemoryStream mstream = new MemoryStream(images);
+                            chat.ava = Image.FromStream(mstream);
+                        }
+                        if (user.Id == idLoggined)
+                        {
+                            chat.isPictureBoxOnlineVisible = true;
+                        }
+                        if (idOnlines != null)
+                        {
+                            for (int i = 0; i < idOnlines.Length; i++)
+                            {
+                                if (user.Id == idOnlines[i])
+                                    chat.isPictureBoxOnlineVisible = true;
+                            }
+                        }
+                        chat.userName = user.Username;
+                        foreach (Control c in chat.Controls)
+                        {
+                            c.Click -= new EventHandler(navigationForm.chatForm.chatPanel_Click);
+                            c.Click += new EventHandler(navigationForm.chatForm.chatPanel_Click);
+                        };
+                        navigationForm.chatForm.flowLayoutPanelListChat.Controls.Add(chat);
+                        });
                     }
                 }
             }
@@ -164,7 +168,8 @@ namespace ClientWinform.SocketHandles
                 byte[] data = new byte[1024];
                 int receivedDataLength = _client.Receive(data);
                 string stringData = Encoding.ASCII.GetString(data, 0, receivedDataLength);
-                UpdateListChat(stringData, form);
+                string[] messages = stringData.Split('\n');
+                UpdateListChat(messages, form);
             }
         }
     }
